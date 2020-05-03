f3d4f87ca13dca5662e68bfca091c1ac754517c8
Chetan Paliwal <chetanpaliwal22@gmail.com>
2020-05-03 09:24:39 -0400

    Valid Parenthesis String

---
+++ 16.java
@@ -0,0 +1,35 @@
+/*Input: "()"
+Output: True
+
+Input: "(*)"
+Output: True
+
+Input: "(*))"
+Output: True
+*/
+class Solution {
+      public boolean checkValidString(String s) {
+        int bal = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '*') {
+                bal++;
+            }
+            else if (--bal< 0) {
+                return false;
+            }
+        }
+        if (bal == 0) {
+            return true;
+        }
+        bal = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) == ')' || s.charAt(i) == '*') {
+                bal++;
+            }
+            else if (--bal< 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
